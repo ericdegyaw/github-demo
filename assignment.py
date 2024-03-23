84ab9a3f4f1ac3046ced5f9b4e0188f105215a90
Eric Degyaw <ericdegyaw@Erics-MacBook-Pro.local>
2024-03-23 11:26:33 +0000

    initial code written

---
+++ assignment.py
@@ -0,0 +1,12 @@
+def addition(x,  y):
+    answer= x+y
+    return answer
+
+name= input("what is you name? : ")
+print (f"Hi {name}, Welcome to the version control and Git Assisgnment!!")
+print ("This will be an addition assignment")
+
+x= float(input("Enter a number: "))
+y= float(input("Enter another number: "))
+
+addition(x,y)
